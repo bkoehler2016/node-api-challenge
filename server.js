bc292dd01e58c3705b53b4a0aae28fbbce5a908d
Ben Koehler <bkoehler85282@gmail.com>
2020-03-20 18:51:55 -0700

    setup client

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const actionRouter = require("./routers/actionRouter");
 const projectRouter = require("./routers/projectRouter");
 
 const server = express();
+const cors = require("cors");
 
 server.use(express.json());
+server.use(cors());
 server.use("/api/actions", actionRouter);
 server.use("/api/projects", projectRouter);
 
