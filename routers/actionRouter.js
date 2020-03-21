0aedce0ddc9b133b41e336dcc6850183052c3ed1
Ben Koehler <bkoehler85282@gmail.com>
2020-03-20 18:29:43 -0700

    completed action router

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -29,4 +29,45 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//Post action to database
+router.post("/", (req, res) => {
+  db.insert(req.body).then(action => {
+    res
+      .status(200)
+      .json(action)
+      .catch(err => {
+        res.status(500).json({ message: "Error posting" });
+      });
+  });
+});
+
+//Update action by id
+router.put("/:id", (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ error: "Name and Description Required" });
+  }
+  db.update(req.params.id, req.body)
+    .then(action => {
+      action
+        ? res.status(200).json(action)
+        : res.status(404).json({ error: "cannot find that post" });
+    })
+    .catch(() => {
+      res.status(500).json({ errorMessage: "Failed to edit" });
+    });
+});
+
+// delete action by ID
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(project => {
+      res.json(project);
+    })
+    .catch(() => {
+      res.status(500).json;
+    });
+});
+
 module.exports = router;
