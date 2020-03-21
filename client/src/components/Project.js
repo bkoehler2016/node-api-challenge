3e01086dbe70a2325b5731ced6f8dbdcb4b59e28
Ben Koehler <bkoehler85282@gmail.com>
2020-03-20 19:17:57 -0700

    tried to add an image

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { Card, CardText, CardBody, CardSubtitle } from "reactstrap";
+import { Card, CardText, CardBody } from "reactstrap";
 const Project = props => {
-  const { name, description, completed } = props.project;
+  const { name, description } = props.project;
 
   return (
     <div>
